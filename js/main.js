f13440b0150af4221100d7aa29ca21269c251ab3
Agustin Paz <agustinpaz180@gmail.com>
2023-09-07 21:40:01 -0300

    añado proyecto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+const confirmado = [];
 const user = JSON.parse(localStorage.getItem('login_success')) || false;
 
 
@@ -40,11 +40,34 @@ function calcular() {
         let diferenciaTiempo = diaS.getTime() - diaE.getTime();  
         let precioPorDia = 5000; 
         let resultado = diferenciaTiempo / (1000 * 60 * 60 * 24);
-        return document.getElementById("result").innerHTML = ` <h3>Precio total por ${resultado} Dia/s $
+        Swal.fire(`Precio total por ${resultado} Dia/s $
+        ${resultado*precioPorDia}`);
+        document.getElementById("result").innerHTML = ` <h3>Precio total por ${resultado} Dia/s $
         ${resultado*precioPorDia} </h3> `;  
     }
 }  
 
+
+const confimar = document.getElementById('confirmar');
+confimar.addEventListener('click', ()=> {
+    let dia1 = document.getElementById("diaInp1").value;  
+    let dia2 = document.getElementById("diaInp2").value;  
+    if(dia1>dia2){
+        return Swal.fire({
+            icon: 'error',
+            title: 'Ops',
+            text: 'Por favor, corrobora las fechas ingresadas',
+          })
+    }else if( (dia1 && dia2)=== '' ){
+        return Swal.fire({
+            icon: 'error',
+            title: 'Ups...',
+            text: 'No se ha ingresado ninguna fecha. Intentalo nuevamente.',
+          })
+    }else{
+        Swal.fire(`Gracias ${user.nombre}. Pronto le llegara la info a su mail :) `);confirmado.push(dia1,dia2);
+    }
+});
 
 let bienvenida = document.getElementById('titulo');
 bienvenida.innerHTML = 
